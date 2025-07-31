3c9f280b624a69e2a1fee09509b8facc9cb5d060
Dariush Mokhlesi <dariushmokhlesi@gmail.com>
2025-07-31 12:47:42 -0500

    Patient.py work

--- src/tables OMOP-CLIF/patient.py
+++ src/tables OMOP-CLIF/patient.py
@@ -7,54 +7,47 @@ from importlib import reload
 from pathlib import Path
 import json
 
-PERSON_COL_NAMES = [
-"person_id", 
-"gender_concept_id", 
-"year_of_birth",
-"month_of_birth",
-"day_of_birth",
-"birth_datetime",
-"race_concept_id",
-"ethnicity_concept_id",
-"location_id",
-"provider_id",
-"care_site_id",
-"person_source_value",
-"gender_source_value",
-"gender_source_concept_id",
-"race_source_value",
-"race_source_concept_id",
-"ethnicity_source_value",
-"ethnicity_source_concept_id",
+PATIENT_COL_NAMES = [
+"patient_id",
+"race_name",
+"ethnicity_name",
+"sex_name",
+"death_dttm",
+"language_name",
+"language_category",
+"sex_cetegory",
+"birth_date",
+"race_category",
+"ethnicity_category",
 ]
 
 with open('config.json', 'r') as f:
     config = json.load(f)
 
 clif_parquet_dir = config["clif_parquet_dir"]
-file_path = Path(clif_parquet_dir) / "clif_patient.parquet"
+file_path = Path(clif_parquet_dir) / "clif_patient.parquet" #CORRECT FILE NEEDED
 
 
-def rename_person():
+def rename_patient():
     try:
-        patient_df = pd.read_parquet(file_path)
-        patient_df = patient_df.rename(columns={'patient_id': 'person_id'})    
-        patient_df = patient_df.rename(columns={'sex_cetegory': 'gender_concept_id'})        
-        patient_df = patient_df.rename(columns={'birth_date': 'year_of_birth'})        
-        patient_df = patient_df.rename(columns={'race_category': 'race_concept_id'})        
-        patient_df = patient_df.rename(columns={'ethnicity_category': 'ethnicity_concept_id'})            
-        print(patient_df.head())
+        person_df = pd.read_parquet(file_path)
+        person_df = person_df.rename(columns={'person_id': 'patient_id'})
+        person_df = person_df.rename(columns={'gender_concept_id': 'sex_cetegory'})        
+        person_df = person_df.rename(columns={'year_of_birth': 'birth_date'})        
+        person_df = person_df.rename(columns={'race_concept_id': 'race_category'})        
+        person_df = person_df.rename(columns={'ethnicity_concept_id': 'ethnicity_category'})             
+        print(person_df.head())
     except Exception as e:
         print(f"Error processing file: {e}")
 
-def adding_columns_person():
+def adding_columns_patient():
     try:
-        patient_df = pd.read_parquet(file_path)
-        patient_df = patient_df.assign(location_id=None, provider_id=None, care_site_id=None, person_source_value=None, gender_source_value=None, gender_source_concept_id=None, race_source_value=None, race_source_concept_id=None, ethnicity_source_value=None, ethnicity_source_concept_id=None)
-        print(patient_df.head())
+        person_df = pd.read_parquet(file_path)
+        person_df = person_df.assign(race_name=None, ethnicity_name=None, sex_name=None, death_dttm=None, language_name=None, language_category=None)
+        print(person_df.head())
     except Exception as e:
         print(f"Error processing file: {e}")
 
 if __name__ == "__main__":
-    rename_person()
-    adding_columns_person()
+    rename_patient()
+    adding_columns_patient()
