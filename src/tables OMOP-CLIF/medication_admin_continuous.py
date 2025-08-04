700d5f684d042e5776cd8015395c27ee0314dc8e
Dariush Mokhlesi <dariushmokhlesi@gmail.com>
2025-08-04 13:25:35 -0500

    Did some medication_admin_continuous

--- src/tables OMOP-CLIF/medication_admin_continuous.py
+++ src/tables OMOP-CLIF/medication_admin_continuous.py
@@ -15,32 +15,48 @@ with open('config.json', 'r') as f:
 
 
 omop_parquet_dir = config["omop_parquet_dir"]
-file_path = Path(omop_parquet_dir) / "clif_medication_admin_continuous.parquet"
+file_path = Path(omop_parquet_dir) / "omop_drug_exposure.parquet"
+output_file = Path(omop_parquet_dir) / "clif_medication_admin_continuous.parquet"
 
 
-def rename_drug_exposure():
+def rename_medication_admin_continuous():
     try:
-        drug_df = pd.read_parquet(file_path)
-        drug_df = drug_df.rename(columns={'med_category': 'drug_concept_id'})  
-        drug_df = drug_df.rename(columns={'admin_dttm': 'drug_exposure_start_datetime'})   
-        drug_df = drug_df.rename(columns={'med_dose': 'quantity'})  
-        drug_df = drug_df.rename(columns={'hospitalization_id': 'visit_occurence_id'})    
-        drug_df = drug_df.rename(columns={'med_name': 'drug_source_value'})   
-        drug_df = drug_df.rename(columns={'med_route_name': 'route_source_value'})   
-        drug_df = drug_df.rename(columns={'med_dose_unit': 'dose_unit_source_value'})   
+        medication_df = pd.read_parquet(file_path)
+        medication_df = medication_df.rename(columns={'drug_concept_id': 'med_category'})  
+        medication_df = medication_df.rename(columns={'drug_exposure_start_datetime': 'admin_dttm'})  
+        medication_df = medication_df.rename(columns={'quantity': 'med_dose'})  
+        medication_df = medication_df.rename(columns={'visit_occurence_id': 'hospitalization_id'})  
+        medication_df = medication_df.rename(columns={'drug_source_value': 'med_name'})  
+        medication_df = medication_df.rename(columns={'route_source_value': 'med_route_name'})  
+        medication_df = medication_df.rename(columns={'dose_unit_source_value': 'med_dose_unit'})  
         
         df = pd.read_csv("MappingMedicationConceptID.csv")
         df['med_category'] = df['med_category'].str.lower().str.strip()
-        mapping = dict(zip(df["med_category"], df["Concept ID"]))
-        drug_df["drug_concept_id"] = drug_df["drug_concept_id"].map(mapping)
-        print(drug_df["drug_concept_id"].unique())
+        mapping = dict(zip(df["Concept ID"], df["med_category"]))
+        print(type(medication_df["med_category"]))
+        print(medication_df["med_category"].head())
+        #medication_df["med_category"] = medication_df["med_category"].map(mapping)
+        medication_df["med_category"] = medication_df["med_category"].apply(
+    lambda x: mapping.get(x, x)
+)
+        print(medication_df["med_category"].unique())
+
     except Exception as e:
         print(f"Error processing file: {e}")
 
-def adding_columns_drug_exposure():
+ 
+def remove_columns_medication_admin_continuous():
     try:
-        drug_df = pd.read_parquet(file_path)
-        drug_df = drug_df.assign(drug_exposure_id=None, 
+        medication_df = pd.read_parquet(file_path)
+        medication_df = medication_df.drop(columns=['drug_exposure_id', 'person_id', 'drug_exposure_start_date', 'drug_exposure_end_date', 'drug_exposure_end_datetime', 'verbatim_end_date', 'drug_type_concept_id', 'stop_reason', 'refills', 'days_supply', 'sig', 'route_concept_id', 'lot_number', 'provider_id', 'visit_detail_id'])
+        print(medication_df.head())
+    except Exception as e:
+        print(f"Error processing file: {e}")
+
+def adding_columns_medication_admin_continuous():
+    try:
+        medication_df = pd.read_parquet(file_path)
+        medication_df = medication_df.assign(drug_exposure_id=None, 
         person_id=None, 
         drug_exposure_start_date = None,
         drug_exposure_end_date = None,
@@ -61,6 +77,6 @@ def adding_columns_drug_exposure():
 
 
 if __name__ == "__main__":
-    rename_drug_exposure()
+    rename_medication_admin_continuous()
     adding_columns_drug_exposure()
 
