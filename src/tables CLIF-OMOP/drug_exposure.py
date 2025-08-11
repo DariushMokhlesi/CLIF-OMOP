35d6cc4807e8732c4ee998c0236dedb1752c6870
Dariush Mokhlesi <dariushmokhlesi@gmail.com>
2025-08-11 15:23:42 -0500

    Did drug_exposure tables

--- src/tables CLIF-OMOP/drug_exposure.py
+++ src/tables CLIF-OMOP/drug_exposure.py
@@ -48,22 +48,40 @@ def rename_drug_exposure():
         drug_df = drug_df.rename(columns={'med_name': 'drug_source_value'})   
         drug_df = drug_df.rename(columns={'med_route_name': 'route_source_value'})   
         drug_df = drug_df.rename(columns={'med_dose_unit': 'dose_unit_source_value'})   
-        
-        print(drug_df.head())
-
+        drug_df['drug_exposure_start_datetime'] = pd.to_datetime(drug_df['drug_exposure_start_datetime'], errors='coerce', utc=True)
+        drug_df['drug_exposure_start_date'] = drug_df['drug_exposure_start_datetime'].dt.date
         df = pd.read_csv("MappingMedicationConceptID.csv")
         df['med_category'] = df['med_category'].str.lower().str.strip()
         mapping = dict(zip(df["med_category"], df["Concept ID"]))
         drug_df["drug_concept_id"] = drug_df["drug_concept_id"].map(mapping)
+
+        drug_df.to_parquet(output_file, index=False)
+        print(drug_df.head())
     except Exception as e:
         print(f"Error renaming to drug exposure file: {e}")
 
+
+def remove_columns_drug_exposure():
+    try:
+        drug_df = pd.read_parquet(output_file)
+        drug_df = drug_df.drop(columns=[
+        'med_order_id',
+        'med_group',
+        'med_route_category',
+        'mar_action_name',
+        'mar_action_category'])
+        drug_df.to_parquet(output_file, index=False)
+        print(drug_df.head())
+    except Exception as e:
+        print(f"Error removing columns to drug exposure file: {e}")
+
+
+
 def adding_columns_drug_exposure():
     try:
-        drug_df = pd.read_parquet(file_path)
+        drug_df = pd.read_parquet(output_file)
         drug_df = drug_df.assign(drug_exposure_id=None, 
         person_id=None, 
-        drug_exposure_start_date = None,
         drug_exposure_end_date = None,
         drug_exposure_end_datetime = None,
         verbatim_end_date = None,
@@ -84,4 +102,5 @@ def adding_columns_drug_exposure():
 
 if __name__ == "__main__":
     rename_drug_exposure()
+    remove_columns_drug_exposure()
     adding_columns_drug_exposure()
